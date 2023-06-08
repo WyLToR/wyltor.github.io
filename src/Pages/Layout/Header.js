3fd75de5001de11ad0498c2de87b86b54be53bdf
WyLToR <bekesi.patrik@gmail.com>
2023-06-08 16:30:17 +0200

    toast container

--- src/Pages/Layout/Header.js
+++ src/Pages/Layout/Header.js
@@ -2,7 +2,14 @@ import { Link } from "react-router-dom";
 import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
 import "../../Style/Header.css"
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 export default function Header() {
+    setTimeout(() => {
+        toast("The webpage is under development")
+        console.log("The webpage is under development, if you saw bug, please notice me on my email")
+    }, 3000)
     return (
         <header>
             <Navbar collapseOnSelect bg="dark" expand="lg" variant="dark">
@@ -11,6 +18,7 @@ export default function Header() {
                     <Navbar.Text className="animated-element"><Link className="link-navbar-menu" to={"/aboutme"}>About Me</Link></Navbar.Text>
                     <Navbar.Text className="animated-element"><Link className="link-navbar-menu" to={"/contact"}>Contact</Link></Navbar.Text>
                     <Navbar.Text className="animated-element"><Link className="link-navbar-menu" to={"/cv"}>CV</Link></Navbar.Text>
+                    <ToastContainer />
                 </Container>
             </Navbar>
         </header>

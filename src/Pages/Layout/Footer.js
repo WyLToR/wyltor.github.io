303bdaf057004e120fa9ee3822d02e75d9d012f2
WyLToR <bekesi.patrik@gmail.com>
2023-06-08 14:56:02 +0200

    delete fetch part commercial uses

--- src/Pages/Layout/Footer.js
+++ src/Pages/Layout/Footer.js
@@ -3,17 +3,17 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import { Link } from 'react-router-dom';
 import "../../Style/Footer.css"
-import { useState, useEffect } from "react"
+import { useState, useEffect, useRef } from "react"
 import Image from 'react-bootstrap/Image';
+import Placeholder from 'react-bootstrap/Placeholder';
 export default function Footer() {
     const [user, setUser] = useState()
+    const [loading, setLoading] = useState(false)
     useEffect(() => {
         fetch("https://api.ipify.org?format=json")
             .then(res => res.json())
-            .then(data => fetch(`http://ipwho.is/${data.ip}`)
-                .then(res => res.json())
-                .then(info=>setUser(info))
-            )
+            .then(data => setUser(data)
+            ).finally(setLoading(true))
     }, [user])
     return (
         <footer>
@@ -21,27 +21,23 @@ export default function Footer() {
                 <Row>
                     <Col><b>Pages:</b></Col>
                     <Col><b>Contact Profiles:</b></Col>
-                    <Col><b>Visitor:</b></Col>
+                    <Col><b>Visitor: {loading ? user?.ip : <Placeholder animation="glow"><Placeholder xs={3} /></Placeholder>}</b></Col>
                 </Row>
                 <Row>
                     <Col><Link className="link-footer" to={"/"}>Main</Link></Col>
-                    <Col><Link className="link-footer" to={"www.codepen.io/WyLToR"}>CodePen</Link></Col>
-                    <Col>IP: {user?.ip}</Col>
+                    <Col xs={8}><Link className="link-footer" to={"www.codepen.io/WyLToR"}>CodePen</Link></Col>
                 </Row>
                 <Row>
                     <Col><Link className="link-footer" to={"/aboutme"}>About Me</Link></Col>
-                    <Col><Link className="link-footer" to="www.linkedin.com/in/bekesi-patrik">Linked In</Link></Col>
-                    <Col>City: {user?.city}</Col>
+                    <Col xs={8}><Link className="link-footer" to="www.linkedin.com/in/bekesi-patrik">Linked In</Link></Col>
                 </Row>
                 <Row>
                     <Col><Link className="link-footer" to={"/contact"}>Contact</Link></Col>
-                    <Col><Link className="link-footer" to={"www.github.com/wyltor"}>GitHub</Link></Col>
-                    <Col>Country: <Image src={user?.flag.img} width="30px"/></Col>
+                    <Col xs={8}><Link className="link-footer" to={"www.github.com/wyltor"}>GitHub</Link></Col>
                 </Row>
                 <Row>
                     <Col><Link className="link-footer" to={"/cv"}>CV</Link></Col>
-                    <Col><Link className="link-footer" to={"mailto:bekesi.patrik@gmail.com"}>Email</Link></Col>
-                    <Col>Continent: {user?.continent}</Col>
+                    <Col xs={8}><Link className="link-footer" to={"mailto:bekesi.patrik@gmail.com"}>Email</Link></Col>
                 </Row>
             </Container>
         </footer>
